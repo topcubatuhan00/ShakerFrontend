27b361f88fabaedb87311810d7522e5eb5aed4cf
Batuhan Topçu <topcubatuhan00@hotmail.com>
2023-12-28 15:47:45 +0300

    filter oprions added

--- src/app/UI/components/home/home.component.ts
+++ src/app/UI/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HomeService } from './services/home.service';
 import { ShakerModel } from './models/shaker.model';
 import { Router } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [],
+	imports: [MatExpansionModule, FormsModule],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.css'
 })
@@ -15,22 +17,46 @@ export class HomeComponent {
 	constructor(
 		private _homeService: HomeService,
 		private _router: Router
-		) { }
-	
+	) { }
+
 	shakerResponse: ShakerModel[] = [];
+	filterResponse: ShakerModel[] = [];
+	panelOpenState = false;
+	isOnlyActive: string = "0";
 
 	ngOnInit(): void {
-		this._homeService.getAllShakers((shakerResponse: ShakerModel[]) => {		
+		this._homeService.getAllShakers((shakerResponse: ShakerModel[]) => {
 			this.shakerResponse = shakerResponse;
+			this.filterResponse = shakerResponse;
 		});
 	}
 
-	addShaker(){
+	addShaker() {
 		this._router.navigateByUrl("/CreateShaker");
 	}
 
-	deleteShaker(shakerId: number){		
+	deleteShaker(shakerId: number) {
 		this._homeService.deleteShaker(shakerId);
 	}
 
+	filterResult() {
+		this.shakerResponse = this.filterResponse;
+		switch (this.isOnlyActive) {
+			case "1":
+				console.log("çalışanı listele");
+				this.shakerResponse = this.shakerResponse.filter(shaker => shaker.status === true);
+				this.panelOpenState = false;
+				break;
+			case "2":
+				console.log("duranı listele");
+				this.shakerResponse = this.shakerResponse.filter(shaker => shaker.status === false);
+				this.panelOpenState = false;
+				break;
+			default:
+				console.log("karışık listele");
+				this.panelOpenState = false;
+				break;
+		}
+		
+	}
 }
